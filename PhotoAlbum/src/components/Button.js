071b6c764a84c2d396904767bb9c3ac276143f9a
samkahchiin <samkahchiin@gmail.com>
2018-04-12 22:52:57 +0800

    feat: Set button text

--- PhotoAlbum/src/components/Button.js
+++ PhotoAlbum/src/components/Button.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
 
-const Button = ({ onPress }) => {
+const Button = ({ onPress, children }) => {
   const { buttonStyle, textStyle } = styles;
 
   return (
     <TouchableOpacity style={buttonStyle} onPress={onPress}>
       <Text style={textStyle}>
-        Buy
+        {children}
       </Text>
     </TouchableOpacity>
   );
